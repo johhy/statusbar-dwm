af385d459fff40cbc1f59862afb87b121f992588
Johhy <johhy1313@gmail.com>
2017-01-15 15:48:27 +0700

    Syntax some edition

--- statusbar.sh
+++ statusbar.sh
@@ -40,24 +40,15 @@ do
 		"00001004") KBD="RU" ;;
 		*) KBD="unknown" ;;
 	esac
-	if [ -n "$(ip addr|grep wlan0|grep UP)" ]
-        then
-		WLAN0="UP"
-                if [ -z $OUT_IP ]
-		then
-			echo "Try getout ip"
-                	OUT_IP=$(curl ipinfo.io/ip)
-		fi
-		if [ -z $LOCAL_IP ]
-		then
-			echo "Try get local ip"
-                	LOCAL_IP=$(ip addr|grep wlan0|grep inet|awk '{print $2}')
-		fi
+	if [ -n "$(ip addr|grep wlan0|grep UP)" ];then
+	    WLAN0="UP"
+      [ -z $OUT_IP ] && OUT_IP=$(curl ipinfo.io/ip)
+		  [ -z $LOCAL_IP ] && LOCAL_IP=$(ip addr|grep wlan0|grep inet|awk '{print $2}')
 	else 
-	        WLAN0="DOWN"
-		OUT_IP=""
-		LOCAL_IP=""
-        fi
+	    WLAN0="DOWN"
+		  OUT_IP=""
+		  LOCAL_IP=""
+  fi
 
 	DATE=$(date +"%a %b %d %T")
 	VOLUME=$(awk -F "[][]" '{print $2}' <(amixer sget Master|grep Right:))        
@@ -70,18 +61,18 @@ do
 	# stat for memory, disk usage ans cpu load
 	MEMORY=$(free -m|grep Mem|awk '{print $4}')
 	DISK=$(df -h|grep "^/"|awk '{print $2-$3}')
-	CPU=$(cat /proc/loadavg|cut -d ' ' -f1)
+	CPU=$(cut -d ' ' -f1 < /proc/loadavg)
 	
 	# net stat
-	LINE=`grep wlan0 /proc/net/dev | sed s/.*://`;
-	RECEIVED2=`echo $LINE | awk '{print $1}'`
-	TRANSMITTED2=`echo $LINE | awk '{print $9}'`
-	TOTAL=$(($RECEIVED2+$TRANSMITTED2))
-	TR=`human_readable $TRANSMITTED2`
-	RS=`human_readable $RECEIVED2`
-	TT=`human_readable $TOTAL`
-	INSPEED=$((($RECEIVED2-$RECEIVED1)/($SLP*1024)))
-	OUTSPEED=$((($TRANSMITTED2-$TRANSMITTED1)/($SLP*1024)))
+	LINE=$(grep wlan0 /proc/net/dev | sed s/.*://)
+	RECEIVED2=$(echo $LINE | awk '{print $1}')
+	TRANSMITTED2=$(echo $LINE | awk '{print $9}')
+	TOTAL=$((RECEIVED2+TRANSMITTED2))
+	TR=$(human_readable "$TRANSMITTED2")
+	RS=$(human_readable "$RECEIVED2")
+	TT=$(human_readable $TOTAL)
+	INSPEED=$(((RECEIVED2-RECEIVED1)/(SLP*1024)))
+	OUTSPEED=$(((TRANSMITTED2-TRANSMITTED1)/(SLP*1024)))
 	# set bottom bar
         BOTTOM="IP_L:$LOCAL_IP  IP_O:$OUT_IP  Tran:$TR  Recv:$RS  Tot:$TT  In:$INSPEED KB/s  Out:$OUTSPEED KB/s  CL:$CPU  MF:$MEMORY MB  DF:$DISK GB"
 	xsetroot -name "$TOP;$BOTTOM"
