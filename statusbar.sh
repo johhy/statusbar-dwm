2041d0c9a8a4e54868acae694380cae7a44a9a58
Johhy <johhy1313@gmail.com>
2017-01-24 21:17:06 +0700

    Close issue #1 - "When press CapsLock on keyboard, statusbar shows keyboard unknown status"

--- statusbar.sh
+++ statusbar.sh
@@ -27,7 +27,7 @@ function get_weather {
     echo "$TEMP C"
 }
 
-#set key api from argument of script 
+#set key api from argument of script
 OWM_KEY=$1
 TMP=$(get_weather)
 
@@ -44,21 +44,23 @@ GWT=$((60*60))
 #Init time for timer
 LAST_T=$(date +%s)
 
-#IP 
+#IP
 OUT_IP=""
 LOCAL_IP=""
 
 while(true)
-do 
+do
 	sleep $SLP
 
   CURRENT_T=$(date +%s)
   #check for weather request
   [ $((CURRENT_T-LAST_T)) -gt $GWT ] && TMP=$(get_weather) && LAST_T=$CURRENT_T
- 
+
 	case "$(xset -q|grep LED| awk '{ print $10 }')" in
 		"00000000") KBD="EN" ;;
+    "00000001") KBD="EN" ;;
 		"00001004") KBD="RU" ;;
+    "00001005") KBD="RU" ;;
 		*) KBD="unknown" ;;
 	esac
 	if ip addr | grep -q 'wlan0.*UP'
@@ -75,17 +77,17 @@ do
 	DATE=$(date +"%a %b %d %T")
 	VOLUME=$(awk -F "[][]" '{print $2}' <(amixer sget Master|grep Right:))
 	MUTE=$(awk -F "[][]" '{print $4}' <(amixer sget Master|grep Right:))
-	if [ "$MUTE" == "off" ];then 
+	if [ "$MUTE" == "off" ];then
 		VOLUME=$MUTE
 	fi
-  
+
 	TOP="Wlan0:$WLAN0 Vol:$VOLUME Kbd:$KBD T:$TMP $DATE"
-	
+
 	# stat for memory, disk usage ans cpu load
 	MEMORY=$(free -m|grep '-'|awk '{print $4}')
 	DISK=$(df -h|grep "^/"|awk '{print $2-$3}')
 	CPU=$(cut -d ' ' -f1 < /proc/loadavg)
-	
+
 	# net stat
 	LINE=$(grep wlan0 /proc/net/dev | sed s/.*://)
 	RECEIVED2=$(echo "$LINE" | awk '{print $1}')
@@ -96,7 +98,7 @@ do
 	OUTSPEED=$(printf "%sit/s" "$(metric $(((TRANSMITTED2-TRANSMITTED1)*8/SLP)))") # in bit/sec
 
 	# set bottom bar
-  BOTTOM="IP_L:$LOCAL_IP  IP_O:$OUT_IP  CPU:$CPU  MF:$MEMORY MB  DF:$DISK GB  Sent:$TR  Recv:$RS  In:$INSPEED  Out:$OUTSPEED" 
+  BOTTOM="IP_L:$LOCAL_IP  IP_O:$OUT_IP  CPU:$CPU  MF:$MEMORY MB  DF:$DISK GB  Sent:$TR  Recv:$RS  In:$INSPEED  Out:$OUTSPEED"
 	xsetroot -name "$TOP;$BOTTOM"
 	TRANSMITTED1=$TRANSMITTED2
 	RECEIVED1=$RECEIVED2
